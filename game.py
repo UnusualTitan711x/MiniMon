8c814b66423168753661a36f54e1d09bff8e9bc1
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-07-25 00:25:35 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,16 +2,22 @@ from textual.app import App
 from textual.widgets import Button, Header, Footer
 
 class EndRunGame(App):
+    BINDINGS = [
+        ("d", "toggle_dark_mode", "Toggle dark mode"),
+    ]
+    
     def compose(self):
         """ The widgets that this app is composed of """
-
-        self.ansi_theme_dark = False
-
+        
         yield Header(show_clock=True)
         yield Footer()
         yield Button("Play")
         yield Button("Quit")
-    pass
+    
+    # Action method here
+    # Should always start with the word "action"
+    def action_toggle_dark_mode(self):
+        self.theme = ("textual-light" if self.theme == "textual-dark" else "textual-dark")
 
 if __name__ == "__main__":
     EndRunGame().run()
