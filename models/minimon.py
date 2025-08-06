b32952ba5be9f3b609f628ad547ee573e359b631
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-08-06 22:40:21 +0100

    Update minimon.py

--- models/minimon.py
+++ models/minimon.py
@@ -1,12 +1,12 @@
 class MiniMon:
-    def __init__(self, name, damage, level, maxHp, attack, defense):
+    def __init__(self, name, level, maxHp, attack, defense):
         self.name = name
-        self.damage = damage
         self.level = level
         self.maxHp = maxHp
         self.currentHp = maxHp
         self.attack = attack
         self.defense = defense
+        self.moves = []
         self.status = None # for paralyzed or collapsed etc
     
     def take_damage(self, damage):
