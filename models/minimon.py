5fcab7c8c090870bdb3c0ac5617f9d8272555c8a
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-08-06 22:00:56 +0100

    fixing folder structure

---
+++ models/minimon.py
@@ -0,0 +1,17 @@
+class MiniMon:
+    def __init__(self, name, damage, level, maxHp, attack, defense):
+        self.name = name
+        self.damage = damage
+        self.level = level
+        self.maxHp = maxHp
+        self.currentHp = maxHp
+        self.attack = attack
+        self.defense = defense
+        self.status = None # for paralyzed or collapsed etc
+    
+    def take_damage(self, damage):
+        effective_damage = max(0, damage - self.defense)
+        self.currentHp -= effective_damage
+        if self.currentHp < 0:
+            self.currentHp = 0
+        return effective_damage
