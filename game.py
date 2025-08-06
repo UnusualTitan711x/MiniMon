4d2b47a354a4e7a229e1a0fb63712b2262d68b7b
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-08-07 00:59:35 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from models.player import Player
+from models.minimon import MiniMon
 from models.example_minimons import BUBBLE, FLAMO, GROG
 import random
 
@@ -7,46 +8,58 @@ class Game:
         self.player = Player(
             name="Player1",
             minimons=[BUBBLE, FLAMO, GROG],
-            current_minimon=BUBBLE
+            active_minimon=BUBBLE
         )
         self.opponent = Player(
             name="Opponent",
             minimons=[FLAMO, GROG],
-            current_minimon=FLAMO
+            active_minimon=FLAMO
         )
 
         self.turn = "player"
         self.battle_log = []
-    
+  
 def main():
     game = Game()
 
-    while game.player.current_minimon.currentHp > 0 and game.opponent.current_minimon.currentHp > 0:
+    while game.player.active_minimon.current_hp > 0 and game.opponent.active_minimon.current_hp > 0:
+
+        print(f"{game.player.active_minimon.name} HP: {game.player.active_minimon.current_hp}")
+        print(f"{game.opponent.active_minimon.name} HP: {game.opponent.active_minimon.current_hp} \n\n")
+
+
         if game.turn == "player":
             # move = game.player.choose_move()
 
             print(f"{game.player.name}'s turn. Choose a move:")
-            print(f"{game.player.current_minimon.name}'s moves: {[move.name for move in game.player.current_minimon.moves]}")
+            print(f"{game.player.active_minimon.name}'s moves: {[move.name for move in game.player.active_minimon.moves]}")
             
             while True:
                 move_name = input("Enter move name: ")
-                if move_name in [m.name for m in game.player.current_minimon.moves]:
+                if move_name in [m.name for m in game.player.active_minimon.moves]:
                     break
             
-            move = next(m for m in game.player.current_minimon.moves if m.name == move_name)
+            move = next(m for m in game.player.active_minimon.moves if m.name == move_name)
 
-            if move in game.player.current_minimon.moves:
-                damage = game.player.current_minimon.use_move(move, game.opponent.current_minimon)
-                game.battle_log.append(f"{game.player.current_minimon.name} used {move.name}")
-                game.turn = "opponent"
+            if move in game.player.active_minimon.moves:
+                damage = game.player.active_minimon.use_move(move, game.opponent.active_minimon)
+                print(f"{game.player.active_minimon.name} used {move.name}")
+
+            if game.opponent.active_minimon.is_fainted():
+                print(f"{game.opponent.active_minimon.name} has fainted!")
+                print(f"{game.player.name} wins!")
+
+            game.turn = "opponent"
         else:
-            move = game.opponent.current_minimon.random.choice(game.opponent.current_minimon.moves)
-            damage = game.player.current_minimon.use_mpve(move, game.player.current_minimon)
-            game.battle_log.append(f"{game.opponent.name} used {move.name} causing {damage} damage.")
+            move = random.choice(game.opponent.active_minimon.moves)
+            damage = game.opponent.active_minimon.use_move(move, game.player.active_minimon)
+            print(f"{game.opponent.name} used {move.name} causing {damage} damage.")
+
+            if game.player.active_minimon.is_fainted():
+                print(f"{game.player.active_minimon.name} has fainted!")
+                print(f"{game.opponent.name} wins!")
+            
             game.turn = "player"
-        
-        print(f"{game.player.current_minimon.name} HP: {game.player.current_minimon.currentHp}")
-        print(f"{game.opponent.current_minimon.name} HP: {game.opponent.current_minimon.currentHp}")
 
 
 main()
