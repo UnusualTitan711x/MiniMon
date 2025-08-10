1eba8052e97c7fc23dcca475935d4c9818331501
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-08-10 20:14:15 +0100

    added data

---
+++ utils/data_loader.py
@@ -0,0 +1,30 @@
+import json
+from models.minimon import MiniMon
+from models.item import Item
+from models.moves import Move
+
+
+with open("data/minimon_data.json") as f:
+    data = json.load(f)
+
+# Load moves into dictionary
+moves_dict = {
+    m["name"]: Move(m["name"], m["power"], m["pp"]) for m in data["moves"]
+}
+
+# load minimons
+all_minimons = []
+
+for m_data in data["minimons"]:
+    minimon_moves = [moves_dict[move_name] for move_name in m_data["moves"]]
+
+    m = MiniMon(
+        name=m_data["name"],
+        max_hp=m_data["max_hp"],
+        attack=m_data["attack"],
+        defense=m_data["defense"],
+        image_text=m_data.get("image_text"),
+        moves=minimon_moves
+    )
+
+    all_minimons.append(m)
